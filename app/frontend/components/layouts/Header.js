f34f6f4a49c6f3f43a6efd9448ab64459573b05b
Vitaly Miroshkin <sysop14@gmail.com>
2021-06-26 22:53:26 +0300

    Localize Header Component

--- app/frontend/components/layouts/Header.js
+++ app/frontend/components/layouts/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
 
 const Header = () => {
+  const { t, i18n } = useTranslation('common')
+
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#">Contacts</Nav.Link>
-          <Nav.Link href="#">New Contact</Nav.Link>
+          <Nav.Link href="#">{t('navi.contacts')}</Nav.Link>
+          <Nav.Link href="#">{t('navi.add_contacts')}</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
